ab340a7787e69848caa696108fa847c0c1c0bf0f
sangkeun kim <sisobus@naver.com>
2015-08-17 20:20:33 +0900

    nginx+flask+virtualenv setting

---
+++ app/__init__.py
@@ -0,0 +1,15 @@
+#-*- coding:utf-8 -*-
+from flask import Flask
+from flask import render_template, flash, redirect, session, url_for, request, g, jsonify
+import os
+import sys
+
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+app = Flask(__name__)
+
+@app.route('/')
+def main():
+    return render_template('main.html')
+
