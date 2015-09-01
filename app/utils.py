e36eb723ec6c83975f6c0abc41d8415f6950482c
Sangkeun Kim <sisobus1@gmail.com>
2015-09-01 16:39:20 +0900

    feed_detail add

--- app/utils.py
+++ app/utils.py
@@ -13,3 +13,8 @@ def createDirectory(directoryName):
         ret = commands.getoutput(command)
         command = 'chmod 777 %s'%directoryName
         ret = commands.getoutput(command)
+
+def get_image_path(real_image_path):
+    ret = ''
+    for t in real_image_path.lstrip().rstrip().split('/')[6:]: ret=ret+t+'/'
+    return ret[:-1]
